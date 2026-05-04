314950f051767b44c5653bc4b6259a0a824a973f
Salah-XD <hudeensala948@gmail.com>
2026-05-04 11:31:05 +0530

    fix

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+export default new Map([
+["src/content/blog/test.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ftest.mdoc&astroContentModuleFlag=true")]]);
+		
